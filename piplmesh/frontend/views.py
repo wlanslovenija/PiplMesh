3b8eb4310f98ce9b198076ef247170b423c56f5d
Andrej Cesen <andrej.cesenn@gmail.com>
2012-02-27 22:27:34 +0100

    Views.py defined

--- piplmesh/frontend/views.py
+++ piplmesh/frontend/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def first(request):
+    return render_to_response('base.html', {'title': 'PiplMesh'})
