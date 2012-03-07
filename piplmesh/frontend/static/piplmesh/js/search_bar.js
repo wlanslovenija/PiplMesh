1b35fef9f39109439616f33b6a81bfe3cc2c7935
Aljaz Prijatelj <aljaz.prijatelj@gmail.com>
2012-03-07 09:48:57 +0100

    Few errors from previous commit fixed.
    Most of the errors fixed, other will be fixed soon.

---
+++ piplmesh/frontend/static/piplmesh/js/search_bar.js
@@ -0,0 +1,9 @@
+google.load('search', '1', {language : 'en'});
+google.setOnLoadCallback(function () {
+    var customSearchOptions = {};
+    var customSearchControl = new google.search.CustomSearchControl('003912915932446183218:zeq20qye9oa', customSearchOptions);
+    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+    var options = new google.search.DrawOptions();
+    options.enableSearchboxOnly("/search");
+    customSearchControl.draw('cse-search-form', options);
+}, true);
---
+++ piplmesh/frontend/static/piplmesh/js/search_page.js
@@ -0,0 +1,29 @@
+google.load('search', '1', {language : 'en'});
+google.setOnLoadCallback(function () {
+    var customSearchOptions = {};
+    var customSearchControl = new google.search.CustomSearchControl('003912915932446183218:zeq20qye9oa', customSearchOptions);
+    customSearchControl.setResultSetSize(google.search.Search.LARGE_RESULTSET);
+    customSearchControl.draw('cse');
+    function parseParamsFromUrl() {
+        var params = {};
+        var parts = window.location.search.substr(1).split('&');
+        for (var i = 0; i < parts.length; i++) {
+        var keyValuePair = parts[i].split('=');
+        var key = decodeURIComponent(keyValuePair[0]);
+        params[key] = keyValuePair[1] ? decodeURIComponent(keyValuePair[1].replace(/\+/g, ' ')) : keyValuePair[1];
+    }
+    return params;
+}
+
+var urlParams = parseParamsFromUrl();
+var queryParamName = "q";
+if (urlParams[queryParamName]) {
+    customSearchControl.execute(urlParams[queryParamName], null,
+    {'oq': urlParams['oq'],
+    'aq': urlParams['aq'],
+    'aqi': urlParams['aqi'],
+    'aql': urlParams['aql'],
+    'gs_sm': urlParams['gs_sm'],
+    'gs_upl': urlParams['gs_upl']});
+    }
+}, true);
