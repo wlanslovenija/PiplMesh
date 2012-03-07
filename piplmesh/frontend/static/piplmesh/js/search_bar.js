268181ba94e42e9860859165a6c39bdef410e735
Aljaz Prijatelj <aljaz.prijatelj@gmail.com>
2012-03-07 15:52:23 +0100

    Added auto complete.

--- piplmesh/frontend/static/piplmesh/js/search_bar.js
+++ piplmesh/frontend/static/piplmesh/js/search_bar.js
@@ -4,6 +4,7 @@ google.setOnLoadCallback(function () {
     var customSearchControl = new google.search.CustomSearchControl('003912915932446183218:zeq20qye9oa', customSearchOptions);
     customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
     var options = new google.search.DrawOptions();
+    options.setAutoComplete(true);
     options.enableSearchboxOnly("/search");
     customSearchControl.draw('cse-search-form', options);
 }, true);
