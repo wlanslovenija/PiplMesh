ee806270cf8c937756ec5352f985593822f7fa0e
Aljaz Prijatelj <aljaz.prijatelj@gmail.com>
2012-03-11 12:44:09 +0100

    Using jQuery for DOM manipulation.

--- piplmesh/frontend/static/piplmesh/js/query.js
+++ piplmesh/frontend/static/piplmesh/js/query.js
@@ -1,3 +1,5 @@
-var query = location.search;
-var url = 'http://www.google.com/search' + query;
-document.getElementById("link").setAttribute("href", url);
+$(document).ready(function () {
+    var query = location.search;
+    var url = 'http://www.google.com/search' + query;
+    $("a#link").prop('href', url);
+});
