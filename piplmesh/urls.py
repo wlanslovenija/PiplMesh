29ab6e64ee919306fbbf4710f86291651bf3d849
Irrialite <crt.kristl@gmail.com>
2012-03-21 01:59:35 +0100

    Converted views to class based generic views.

--- piplmesh/urls.py
+++ piplmesh/urls.py
@@ -13,11 +13,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     # Registration, login, logout
-    url(r'^register/$', account_views.registration_view, name='registration'),
+    url(r'^register/$', account_views.RegistrationView.as_view(), name='registration'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='logout'),
     # Facebook
-    url(r'^facebook/login/$', account_views.facebook_login, name='facebook_login'),
-    url(r'^facebook/logout/$', account_views.facebook_logout, name='facebook_logout'),
-    url(r'^facebook/callback/$', account_views.facebook_callback, name='facebook_callback'),
+    url(r'^facebook/login/$', account_views.FacebookLoginView.as_view(), name='facebook_login'),
+    url(r'^facebook/logout/$', account_views.FacebookLogoutView.as_view(), name='facebook_logout'),
+    url(r'^facebook/callback/$', account_views.FacebookCallbackView.as_view(), name='facebook_callback'),
 )
