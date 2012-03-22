b4e5618773abbdabc06e5d7efa4ec4cf3be4a77e
Irrialite <crt.kristl@gmail.com>
2012-03-22 15:08:21 +0100

    Last clean-up. Probably.

--- piplmesh/account/views.py
+++ piplmesh/account/views.py
@@ -5,8 +5,7 @@ from django.conf import settings
 from django.contrib import auth
 from django.core.urlresolvers import reverse
 from django.views import generic as generic_views
-from django.views.generic import simple
-from django.views.generic import edit as edit_views
+from django.views.generic import simple, edit as edit_views
 
 from piplmesh.account import forms
 
@@ -22,23 +21,18 @@ class RegistrationView(edit_views.FormView):
     # Have to do this because we don't have reverse_lazy() yet
     def get_success_url(self):
         return reverse('home')
-      
+
+    def form_valid(self, form):
+        username, password = form.save()
+        new_user = auth.authenticate(username=username, password=password)
+        auth.login(self.request, new_user)
+        super(RegistrationView, self).form_valid(self, form)
+
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
             return simple.redirect_to(request, url=self.get_success_url(), permanent=False)
         super(RegistrationView, self).get(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        if form.is_valid():
-            username, password = form.save()
-            new_user = auth.authenticate(username=username, password=password)
-            auth.login(request, new_user)
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-     
 class FacebookLoginView(generic_views.RedirectView):
     """ 
     This view authenticates the user via Facebook. 
