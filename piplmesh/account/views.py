941112a5d8ccdc8df0349aa551774fb86b26ecfe
Aljaz Prijatelj <aljaz.prijatelj@gmail.com>
2012-03-29 15:43:46 +0200

    Included inital_accepts_request function.

--- piplmesh/account/views.py
+++ piplmesh/account/views.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, redirect
 from django.template import context
 
-from piplmesh.account import forms
+from piplmesh.account import forms, utils
 
 def registration_view(request):
     """
@@ -17,9 +17,9 @@ def registration_view(request):
 
     if request.user.is_authenticated():
         return redirect('home')
-    form = forms.RegistrationForm()
+    form = utils.initial_accepts_request(request, forms.RegistrationForm())
     if request.method == 'POST':
-        form = forms.RegistrationForm(request.POST)
+        form = utils.initial_accepts_request(request, forms.RegistrationForm(request.POST))
         if form.is_valid():
             username, password = form.save()
             new_user = auth.authenticate(username=username, password=password)
