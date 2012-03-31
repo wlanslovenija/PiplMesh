240145cc3abd13cf72b8a8a014753fee23bb9bd6
agiz <ziga.zupanec@gmail.com>
2012-03-31 14:36:53 +0200

    Added GenderField.

--- piplmesh/account/fields.py
+++ piplmesh/account/fields.py
@@ -2,6 +2,11 @@ from django.conf import settings
 
 import mongoengine
 
+GENDER_CHOICES = (
+    ('male', _('Male')),
+    ('female', _('Female'))
+)
+
 def get_initial_language(request=None):
     return settings.LANGUAGE_CODE
 
@@ -12,3 +17,11 @@ class LanguageField(mongoengine.StringField):
         kwargs.setdefault('default', get_initial_language)
         
         super(LanguageField, self).__init__(*args, **kwargs)
+
+class GenderField(mongoengine.StringField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 6)
+        kwargs.setdefault('choices', GENDER_CHOICES)
+        kwargs.setdefault('default', None)
+        
+        super(GenderField, self).__init__(*args, **kwargs)
