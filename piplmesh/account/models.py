6ada5a5f36b4629df2f95af98f0106f460804a5f
agiz <ziga.zupanec@gmail.com>
2012-03-31 13:46:39 +0200

    Defined language field.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -4,8 +4,11 @@ from mongoengine.django import auth
 from piplmesh.account import fields
 
 class User(auth.User):
+    # TODO: Add constraints
     birthdate = mongoengine.DateTimeField()
+    # TODO: Add constraints
     gender = mongoengine.StringField(max_length=6)
+    language = fields.LanguageField()
+    
     facebook_id = mongoengine.IntField()
     facebook_token = mongoengine.StringField(max_length=150)
-    language = mongoengine.StringField(max_length=5)
