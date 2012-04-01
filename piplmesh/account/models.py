327923d080fa23073f666ac6fe50806467d2fd7a
agiz <ziga.zupanec@gmail.com>
2012-04-01 10:07:56 +0200

    Added LimitedDateTimeField limiting arguments.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -1,10 +1,12 @@
+import datetime
+
 import mongoengine
 from mongoengine.django import auth
 
 from piplmesh.account import fields
 
 class User(auth.User):
-    birthdate = fields.LimitedDateTimeField()
+    birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today())
     gender = fields.GenderField()
     language = fields.LanguageField()
     
