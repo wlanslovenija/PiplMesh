dd21a2c3850eac6a6be915a4e33e8ecad8ecd3fb
agiz <ziga.zupanec@gmail.com>
2012-04-01 10:53:51 +0200

    Added lower_limit constraint.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -6,7 +6,7 @@ from mongoengine.django import auth
 from piplmesh.account import fields
 
 class User(auth.User):
-    birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today())
+    birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today(), lower_limit=datetime.datetime.today() - datetime.timedelta(366 * 120))
     gender = fields.GenderField()
     language = fields.LanguageField()
     
