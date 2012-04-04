976304c6ce20ed5ae549403a077242d9f5f70b9e
Miha Novak <m.miha.novak@gmail.com>
2012-04-04 13:06:04 +0200

    Added signal for user login message.

--- piplmesh/account/signals.py
+++ piplmesh/account/signals.py
@@ -2,7 +2,17 @@ from django import dispatch
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 
+user_login = dispatch.Signal(providing_args=["request", "user"])
 user_logout = dispatch.Signal(providing_args=["request", "user"])
+
+def user_login_message(sender, request, user, **kwargs):
+    """
+    Show success login message.
+    """
+    
+    messages.success(request, _("You have been successfully logged in."), fail_silently=True)
+
+user_login.connect(user_login_message, dispatch_uid=__name__ + '.user_login_message')
 
 def user_logout_message(sender, request, user, **kwargs):
     """
