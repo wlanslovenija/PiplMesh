258156ee2b34b72a6702e418066b09770466e4ee
agiz <ziga.zupanec@gmail.com>
2012-04-05 10:19:46 +0200

    Added global date lower limit constant.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -5,8 +5,11 @@ from mongoengine.django import auth
 
 from piplmesh.account import fields
 
+# rough lower limit date
+LOWER_DATE_LIMIT = 366 * 120
+
 class User(auth.User):
-    birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today(), lower_limit=datetime.datetime.today() - datetime.timedelta(366 * 120))
+    birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today(), lower_limit=datetime.datetime.today() - datetime.timedelta(LOWER_DATE_LIMIT))
     gender = fields.GenderField()
     language = fields.LanguageField()
     
