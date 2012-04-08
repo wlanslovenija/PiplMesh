a5afd5530ccd3d883951a99b195760b7ac673518
agiz <ziga.zupanec@gmail.com>
2012-04-08 22:53:27 +0200

    Changed __init__ errors with AttributeError, added bound error to default_error_messages.

--- piplmesh/account/formfields.py
+++ piplmesh/account/formfields.py
@@ -6,20 +6,16 @@ from django.utils.translation import ugettext_lazy as _
 from piplmesh.account import fields
 
 class LimitedDateTimeField(forms.DateTimeField):
-    default_error_messages = {
-        'bounds': _(u"Value is out of bounds."),
-        'syntax_lower': _(u"Invalid lower_limit argument."),
-        'syntax_upper': _(u"Invalid upper_limit argument."),
-    }
+    forms.DateTimeField.default_error_messages['bounds'] = _(u"Value is out of bounds.")
 
     def __init__(self, upper_limit=None, lower_limit=None, input_formats=None, *args, **kwargs):
         self.upper_limit = upper_limit
         self.lower_limit = lower_limit
 
         if self.upper_limit and not isinstance(self.upper_limit, (datetime.datetime, datetime.date)):
-            raise forms.ValidationError(self.error_messages['syntax_upper'])
+            raise AttributeError
         if self.lower_limit and not isinstance(self.lower_limit, (datetime.datetime, datetime.date)):
-            raise forms.ValidationError(self.error_messages['syntax_lower'])
+            raise AttributeError
 
         super(LimitedDateTimeField, self).__init__(input_formats=None, *args, **kwargs)
 
