e37750ae9d0433d3d879375feb4f670a9ff6a80e
agiz <ziga.zupanec@gmail.com>
2012-04-08 18:25:43 +0200

    Fixed error reporting and clean() method.

--- piplmesh/account/formfields.py
+++ piplmesh/account/formfields.py
@@ -2,45 +2,27 @@ import datetime
 
 from django import forms
 
-class FormLimitedDateTimeField(forms.DateTimeField):
+from piplmesh.account import models
+
+class LimitedDateTimeField(forms.DateTimeField):
     def __init__(self, upper_limit=None, lower_limit=None, input_formats=None, *args, **kwargs):
         self.upper_limit = upper_limit
         self.lower_limit = lower_limit
 
         if self.upper_limit and not isinstance(self.upper_limit, (datetime.datetime, datetime.date)):
-            self.error(u'Invalid upper_limit argument.')
+            raise ValidationError(models.error_messages['syntax_upper'])
         if self.lower_limit and not isinstance(self.lower_limit, (datetime.datetime, datetime.date)):
-            self.error(u'Invalid lower_limit argument.')
+            raise ValidationError(models.error_messages['syntax_lower'])
 
         super(FormLimitedDateTimeField, self).__init__(input_formats=None, *args, **kwargs)
    
+
     def clean(self, value):
-        super(FormLimitedDateTimeField, self).clean(value)
-        
-        try:
-            birthdate = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
-        except ValueError:
-            birthdate = datetime.datetime.strptime(value, '%Y-%m-%d')
-            
-        
-        if self.upper_limit:
-            tmp_value = birthdate
-            tmp_upper_limit = self.upper_limit
-            if not isinstance(birthdate, datetime.datetime) or not isinstance(self.upper_limit, datetime.datetime):
-                if isinstance(self.upper_limit, datetime.datetime):
-                    tmp_upper_limit = self.upper_limit.date()
-                elif isinstance(tmp_value, datetime.datetime):
-                    tmp_value = birthdate.date()
-            if tmp_value > tmp_upper_limit:
-                self.error(u'Value is out of bounds.')
-                    
-        if self.lower_limit:
-            tmp_value = birthdate
-            tmp_lower_limit = self.lower_limit
-            if not isinstance(birthdate, datetime.datetime) or not isinstance(self.lower_limit, datetime.datetime):
-                if isinstance(self.lower_limit, datetime.datetime):
-                    tmp_lower_limit = self.lower_limit.date()
-                elif isinstance(tmp_value, datetime.datetime):
-                    tmp_value = birthdate.date()
-            if tmp_value < tmp_lower_limit:
-                self.error(u'Value is out of bounds.')
+        value = super(LimitedDateTimeField, self).clean(value)
+
+        def error():
+            raise ValidationError(models.error_messages['bounds'])
+
+        models.limit_date(value, self.lower_limit, self.upper_limit, error)
+
+        return value
