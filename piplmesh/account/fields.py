feba97c2b55520e7ad0ab1f4315c0268c27ecc57
agiz <ziga.zupanec@gmail.com>
2012-04-08 19:14:07 +0200

    Moved form constants here.

--- piplmesh/account/fields.py
+++ piplmesh/account/fields.py
@@ -1,10 +1,43 @@
 import datetime
 
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 import mongoengine
 
-from piplmesh.account import models
+GENDER_CHOICES = (
+    ('male', _('male')),
+    ('female', _('female'))
+)
+
+error_messages = {
+    'bounds': _(u"Value is out of bounds."),
+    'syntax_lower': _(u"Invalid lower_limit argument."),
+    'syntax_upper': _(u"Invalid upper_limit argument."),
+}
+
+def limit_date(value, lower_limit, upper_limit, error):
+    if upper_limit:
+        tmp_value = value
+        tmp_upper_limit = upper_limit
+        if not isinstance(value, datetime.datetime) or not isinstance(upper_limit, datetime.datetime):
+            if isinstance(upper_limit, datetime.datetime):
+                tmp_upper_limit = upper_limit.date()
+            elif isinstance(tmp_value, datetime.datetime):
+                tmp_value = value.date()
+        if tmp_value > tmp_upper_limit:
+            error()
+                
+    if lower_limit:
+        tmp_value = value
+        tmp_lower_limit = lower_limit
+        if not isinstance(value, datetime.datetime) or not isinstance(lower_limit, datetime.datetime):
+            if isinstance(lower_limit, datetime.datetime):
+                tmp_lower_limit = lower_limit.date()
+            elif isinstance(tmp_value, datetime.datetime):
+                tmp_value = value.date()
+        if tmp_value < tmp_lower_limit:
+            error()
 
 def get_initial_language(request=None):
     return settings.LANGUAGE_CODE
@@ -20,8 +53,8 @@ class LanguageField(mongoengine.StringField):
 class GenderField(mongoengine.StringField):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('max_length', 6)
-        kwargs.setdefault('choices', models.GENDER_CHOICES)
-        kwargs.setdefault('default', models.GENDER_CHOICES[1][0])
+        kwargs.setdefault('choices', GENDER_CHOICES)
+        kwargs.setdefault('default', GENDER_CHOICES[1][0])
         
         super(GenderField, self).__init__(*args, **kwargs)
 
@@ -31,9 +64,9 @@ class LimitedDateTimeField(mongoengine.DateTimeField):
         self.lower_limit = lower_limit
 
         if self.upper_limit and not isinstance(self.upper_limit, (datetime.datetime, datetime.date)):
-            self.error(models.error_messages['syntax_upper'])
+            self.error(error_messages['syntax_upper'])
         if self.lower_limit and not isinstance(self.lower_limit, (datetime.datetime, datetime.date)):
-            self.error(models.error_messages['syntax_lower'])
+            self.error(error_messages['syntax_lower'])
 
         super(LimitedDateTimeField, self).__init__(*args, **kwargs)
    
@@ -41,6 +74,6 @@ class LimitedDateTimeField(mongoengine.DateTimeField):
         super(LimitedDateTimeField, self).validate(value)
 
         def error():
-            raise self.error(models.error_messages['bounds'])
+            raise self.error(error_messages['bounds'])
         
-        models.limit_date(value, self.lower_limit, self.upper_limit, error)
+        limit_date(value, self.lower_limit, self.upper_limit, error)
