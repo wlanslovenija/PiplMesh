e98ac745ecca71591bbb441baaf9b88b98fab8da
agiz <ziga.zupanec@gmail.com>
2012-04-08 18:28:08 +0200

    Added limit_date top-level function, defined gender and translatable error reporting variables.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -1,5 +1,7 @@
 import datetime
 
+from django.utils.translation import ugettext_lazy as _
+
 import mongoengine
 from mongoengine.django import auth
 
@@ -7,6 +9,41 @@ from piplmesh.account import fields
 
 # rough lower limit date
 LOWER_DATE_LIMIT = 366 * 120
+
+GENDER_CHOICES = (
+    ('male', _('male')),
+    ('female', _('female'))
+)
+
+error_messages = {
+    'bounds': _(u"Value is out of bounds."),
+    'syntax_lower': _(u"Invalid lower_limit argument."),
+    'syntax_upper': _(u"Invalid upper_limit argument."),
+}
+
+
+def limit_date(value, lower_limit, upper_limit, error):
+    if upper_limit:
+        tmp_value = value
+        tmp_upper_limit = upper_limit
+        if not isinstance(value, datetime.datetime) or not isinstance(upper_limit, datetime.datetime):
+            if isinstance(upper_limit, datetime.datetime):
+                tmp_upper_limit = upper_limit.date()
+            elif isinstance(tmp_value, datetime.datetime):
+                tmp_value = value.date()
+        if tmp_value > tmp_upper_limit:
+            error()
+                
+    if lower_limit:
+        tmp_value = value
+        tmp_lower_limit = lower_limit
+        if not isinstance(value, datetime.datetime) or not isinstance(lower_limit, datetime.datetime):
+            if isinstance(lower_limit, datetime.datetime):
+                tmp_lower_limit = lower_limit.date()
+            elif isinstance(tmp_value, datetime.datetime):
+                tmp_value = value.date()
+        if tmp_value < tmp_lower_limit:
+            error()
 
 class User(auth.User):
     birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today(), lower_limit=datetime.datetime.today() - datetime.timedelta(LOWER_DATE_LIMIT))
