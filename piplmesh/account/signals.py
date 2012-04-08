f21c06cea86fdbb8687e8ba00d7cf748b02d0f21
Miha Novak <m.miha.novak@gmail.com>
2012-04-08 19:34:40 +0200

    Used signals from django.contrib.auth.signals. We show message if user logs in or logs out.

--- piplmesh/account/signals.py
+++ piplmesh/account/signals.py
@@ -1,9 +1,7 @@
 from django import dispatch
+from django.contrib.auth import signals
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
-
-user_login = dispatch.Signal(providing_args=["request", "user"])
-user_logout = dispatch.Signal(providing_args=["request", "user"])
 
 def user_login_message(sender, request, user, **kwargs):
     """
@@ -12,7 +10,7 @@ def user_login_message(sender, request, user, **kwargs):
     
     messages.success(request, _("You have been successfully logged in."), fail_silently=True)
 
-user_login.connect(user_login_message, dispatch_uid=__name__ + '.user_login_message')
+signals.user_logged_in.connect(user_login_message, dispatch_uid=__name__ + '.user_login_message')
 
 def user_logout_message(sender, request, user, **kwargs):
     """
@@ -21,4 +19,4 @@ def user_logout_message(sender, request, user, **kwargs):
     
     messages.success(request, _("You have been successfully logged out."), fail_silently=True)
 
-user_logout.connect(user_logout_message, dispatch_uid=__name__ + '.user_logout_message')
+signals.user_logged_out.connect(user_logout_message, dispatch_uid=__name__ + '.user_logout_message')
