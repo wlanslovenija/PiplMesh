42ce31b357ad9958f95b7aea84b63b3e2d2658a2
agiz <ziga.zupanec@gmail.com>
2012-04-08 19:14:31 +0200

    Fixed raising ValidationError.

--- piplmesh/account/formfields.py
+++ piplmesh/account/formfields.py
@@ -2,7 +2,7 @@ import datetime
 
 from django import forms
 
-from piplmesh.account import models
+from piplmesh.account import fields
 
 class LimitedDateTimeField(forms.DateTimeField):
     def __init__(self, upper_limit=None, lower_limit=None, input_formats=None, *args, **kwargs):
@@ -10,19 +10,19 @@ class LimitedDateTimeField(forms.DateTimeField):
         self.lower_limit = lower_limit
 
         if self.upper_limit and not isinstance(self.upper_limit, (datetime.datetime, datetime.date)):
-            raise ValidationError(models.error_messages['syntax_upper'])
+            raise forms.ValidationError(fields.error_messages['syntax_upper'])
         if self.lower_limit and not isinstance(self.lower_limit, (datetime.datetime, datetime.date)):
-            raise ValidationError(models.error_messages['syntax_lower'])
+            raise forms.ValidationError(fields.error_messages['syntax_lower'])
 
-        super(FormLimitedDateTimeField, self).__init__(input_formats=None, *args, **kwargs)
+        super(LimitedDateTimeField, self).__init__(input_formats=None, *args, **kwargs)
    
 
     def clean(self, value):
         value = super(LimitedDateTimeField, self).clean(value)
 
         def error():
-            raise ValidationError(models.error_messages['bounds'])
+            raise forms.ValidationError(fields.error_messages['bounds'])
 
-        models.limit_date(value, self.lower_limit, self.upper_limit, error)
+        fields.limit_date(value, self.lower_limit, self.upper_limit, error)
 
         return value
