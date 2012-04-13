67bf3ac1d83c8691150b34ee25db988c41026e59
agiz <ziga.zupanec@gmail.com>
2012-04-13 21:25:21 +0200

    Added fields for user status.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -11,6 +11,9 @@ class User(auth.User):
     birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today(), lower_limit=datetime.datetime.today() - datetime.timedelta(LOWER_DATE_LIMIT))
     gender = fields.GenderField()
     language = fields.LanguageField()
+    clist = mongoengine.ListField()
+    dlist = mongoengine.ListField()
+    logged = mongoengine.BooleanField(default=False)
     
     facebook_id = mongoengine.IntField()
     facebook_token = mongoengine.StringField(max_length=150)
