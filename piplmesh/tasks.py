c740348a70c1fdfc42d7e69c9a86ec0f251d8907
agiz <ziga.zupanec@gmail.com>
2012-04-14 11:23:40 +0200

    Removed return.

--- piplmesh/tasks.py
+++ piplmesh/tasks.py
@@ -6,26 +6,24 @@ from piplmesh.account import models
 
 @task
 def prune_users():
-    print "tasks add"
-    
     chan_users = models.User.objects()
     for user in chan_users:
         cl = user.clist
         dl = user.dlist
-        print cl
-        print dl
+        #print cl
+        #print dl
         for el in dl:
             try:
                 cl.remove(el)
             except ValueError:
                 pass
-        print len(cl)
+        #print len(cl)
 
         if len(cl) == 0 and len(dl) > 0:
+            # TODO: Implement joining/parting subsystem
             channel_id = 'a'
             for el in dl:
                 user.update(pull__clist=el)
                 user.update(pull__dlist=el)
             user.update(set__logged=False)
             updates.send_update(channel_id, {'type':'answer', 'value':{'action':'PART', 'message':user.username}})
-    return x + y
