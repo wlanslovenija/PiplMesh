03016ae51ff999d8bb000b2370ffc7d9fa92ea10
agiz <ziga.zupanec@gmail.com>
2012-04-16 17:58:48 +0200

    Removed Channel document.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -7,13 +7,18 @@ from piplmesh.account import fields
 
 LOWER_DATE_LIMIT = 366 * 120
 
+class Connection(mongoengine.EmbeddedDocument):
+    http_if_none_match = mongoengine.StringField()
+    http_if_modified_since = mongoengine.StringField()
+
 class User(auth.User):
     birthdate = fields.LimitedDateTimeField(upper_limit=datetime.datetime.today(), lower_limit=datetime.datetime.today() - datetime.timedelta(LOWER_DATE_LIMIT))
     gender = fields.GenderField()
     language = fields.LanguageField()
-    clist = mongoengine.ListField()
-    dlist = mongoengine.ListField()
-    logged = mongoengine.BooleanField(default=False)
-    
+
     facebook_id = mongoengine.IntField()
     facebook_token = mongoengine.StringField(max_length=150)
+
+    opened_connections = mongoengine.ListField(mongoengine.EmbeddedDocumentField(Connection))
+    channel = mongoengine.DictField()
+    last_access = mongoengine.DateTimeField()
