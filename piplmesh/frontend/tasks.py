e26a501b17ad654a06bd982d11698d16bc35d473
agiz <ziga.zupanec@gmail.com>
2012-04-19 17:31:41 +0200

    Replaced hard-coded default channel with constant.

--- piplmesh/frontend/tasks.py
+++ piplmesh/frontend/tasks.py
@@ -2,39 +2,39 @@ import datetime
 
 from django.conf import settings
 
-from celery.task import task
+from celery import task
 
 from pushserver.utils import updates
 
 from piplmesh.account import models
+from piplmesh.frontend import views
 
 CHECK_ONLINE_USERS_RECONNECT_TIMEOUT = 2 * settings.CHECK_ONLINE_USERS_INTERVAL
 
-@task
+@task.task
 def check_online_users():
     for user in models.User.objects():
-        channel_id = 'a'
-        if models.User.objects( \
-            id=user.id, \
-            is_online=True, \
-            connections=[], \
+        if models.User.objects(
+            id=user.id,
+            is_online=True,
+            connections=[],
             connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT), \
         ).update(set__is_online=False):
             updates.send_update(
-                channel_id,
+                views.HOME_CHANNEL_ID,
                 {
                     'type': 'userlist',
                     'action': 'PART',
                     'username': user.username,
                 }
             )
-        elif models.User.objects( \
-            id=user.id, \
-            is_online=False, \
-            connections__ne=[], \
+        elif models.User.objects(
+            id=user.id,
+            is_online=False,
+            connections__ne=[],
         ).update(set__is_online=True):
             updates.send_update(
-                channel_id,
+                views.HOME_CHANNEL_ID,
                 {
                     'type': 'userlist',
                     'action': 'JOIN',
