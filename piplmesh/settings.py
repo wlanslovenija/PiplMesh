f513aac768587b39921f916b5d85329d9a2376ad
agiz <ziga.zupanec@gmail.com>
2012-04-19 17:47:52 +0200

    Changed taskmeta_collection to celery_taskmeta.

--- piplmesh/settings.py
+++ piplmesh/settings.py
@@ -190,7 +190,7 @@ CELERY_MONGODB_BACKEND_SETTINGS = {
     'host': '127.0.0.1',
     'port': 27017,
     'database': 'celery',
-    'taskmeta_collection': 'messages',
+    'taskmeta_collection': 'celery_taskmeta',
 }
 
 BROKER_BACKEND = 'mongodb'
