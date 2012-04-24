84c926cf907faf9b8c91cd92a995d38491ef9c2b
Mitar <mitar.git@tnode.com>
2012-04-24 03:17:40 +0200

    Upgraded to newer version of pushserver.

--- piplmesh/frontend/static/piplmesh/js/home.js
+++ piplmesh/frontend/static/piplmesh/js/home.js
@@ -1,13 +1,13 @@
 function updateUserlist(data) {
-    if (data.action == 'JOIN') {
-        $('<li/>').text(data.username).appendTo('#userlist');
-    }
-    else if (data.action == 'PART') {
-        // TODO: Improve escape and use more suitable :contains method
-        $('#userlist li:contains(' + escape(data.username) + ')').remove();
-    }
+	if (data.action == 'JOIN') {
+		$('<li/>').text(data.username).appendTo('#userlist');
+	}
+	else if (data.action == 'PART') {
+		// TODO: Improve escape and use more suitable :contains method
+		$('#userlist li:contains(' + escape(data.username) + ')').remove();
+	}
 }
 
 $(document).ready(function () {
-    registerUpdatesProcessor('userlist', updateUserlist);
+	$.updates.registerProcessor('home_channel', 'userlist', updateUserlist);
 });
