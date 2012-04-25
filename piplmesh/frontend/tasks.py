6ff70effbb291f4a580f47d1129f892aac32f167
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-04-25 10:51:18 +0200

    Fixed TODO: query is limited so there's no iterating over all users

--- piplmesh/frontend/tasks.py
+++ piplmesh/frontend/tasks.py
@@ -14,26 +14,23 @@ CHECK_ONLINE_USERS_RECONNECT_TIMEOUT = 2 * settings.CHECK_ONLINE_USERS_INTERVAL
 @task.task
 def check_online_users():
     # TODO: Iterating over all users in Python could become really slow once there are millions of users, much better is to limit the query only to potentially interesting users (conditions are already bellow)
-    for user in models.User.objects():
-        if models.User.objects(
-            id=user.id,
+    for user in [users for users in models.User.objects(
             is_online=True,
             connections=[],
             connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
-        ).update(set__is_online=False):
-            updates.send_update(
+            ) if users.update(set__is_online=False)]:
+        updates.send_update(
                 views.HOME_CHANNEL_ID,
                 {
                     'type': 'userlist',
                     'action': 'PART',
                     'username': user.username,
                 }
-            )
-        elif models.User.objects(
-            id=user.id,
+        )
+    for user in [users for users in models.User.objects(
             is_online=False,
             connections__ne=[],
-        ).update(set__is_online=True):
+            ) if users.update(set__is_online=True)]:
             updates.send_update(
                 views.HOME_CHANNEL_ID,
                 {
