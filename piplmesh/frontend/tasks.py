a270852929b59552bfc9e3e73921178cc780a3fe
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-05-11 13:17:06 +0200

    Returned update query to previous state, limited query

--- piplmesh/frontend/tasks.py
+++ piplmesh/frontend/tasks.py
@@ -13,13 +13,17 @@ CHECK_ONLINE_USERS_RECONNECT_TIMEOUT = 2 * settings.CHECK_ONLINE_USERS_INTERVAL
 
 @task.task
 def check_online_users():
-    # TODO: Iterating over all users in Python could become really slow once there are millions of users, much better is to limit the query only to potentially interesting users (conditions are already bellow)
     for user in models.User.objects(
-            is_online=True,
-            connections=[],
-            connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
-            ):
-        if user.update(set__is_online=False):
+		is_online=True,
+		connections=[],
+		connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
+	):
+		if models.User.objects( 
+			id=user.id,
+			is_online=True,
+			connections=[],
+			connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
+		).update(set__is_online=False):
             updates.send_update(
                 views.HOME_CHANNEL_ID,
                 {
@@ -27,12 +31,16 @@ def check_online_users():
                     'action': 'PART',
                     'username': user.username,
                 }
-        )
+			)
     for user in models.User.objects(
+		is_online=False,
+		connections__ne=[],
+	):
+        if models.User.objects(
+            id=user.id,
             is_online=False,
             connections__ne=[],
-            ):
-        if user.update(set__is_online=True):
+        ).update(set__is_online=True):
             updates.send_update(
                 views.HOME_CHANNEL_ID,
                 {
