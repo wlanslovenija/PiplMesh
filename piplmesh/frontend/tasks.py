3a26dcf6da2063baa4d6bda3323856f155a5fc57
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-05-11 19:52:26 +0200

    Indentation fixed, for loops flipped

--- piplmesh/frontend/tasks.py
+++ piplmesh/frontend/tasks.py
@@ -13,39 +13,40 @@ CHECK_ONLINE_USERS_RECONNECT_TIMEOUT = 2 * settings.CHECK_ONLINE_USERS_INTERVAL
 
 @task.task
 def check_online_users():
-	for user in models.User.objects(
-		is_online=True,
-		connections=[],
-		connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
-	):
-		if models.User.objects( 
-			id=user.id,
-			is_online=True,
-			connections=[],
-			connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
-		).update(set__is_online=False):
-			updates.send_update(
-				views.HOME_CHANNEL_ID,
-				{
-					'type': 'userlist',
-					'action': 'PART',
-					'username': user.username,
-				}
-			)
-	for user in models.User.objects(
-		is_online=False,
-		connections__ne=[],
-	):
-		if models.User.objects(
-			id=user.id,
-			is_online=False,
-			connections__ne=[],
-		).update(set__is_online=True):
-			updates.send_update(
-				views.HOME_CHANNEL_ID,
-				{
-					'type': 'userlist',
-					'action': 'JOIN',
-					'username': user.username,
-				}
-			)
+    for user in models.User.objects(
+        is_online=False,
+        connections__ne=[],
+    ):
+        if models.User.objects(
+            id=user.id,
+            is_online=False,
+            connections__ne=[],
+        ).update(set__is_online=True):
+            updates.send_update(
+                views.HOME_CHANNEL_ID,
+                    {
+                    'type': 'userlist',
+                    'action': 'JOIN',
+                    'username': user.username,
+                    }
+            )
+
+    for user in models.User.objects(
+        is_online=True,
+        connections=[],
+        connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
+    ):
+        if models.User.objects(
+            id=user.id,
+            is_online=True,
+            connections=[],
+            connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
+        ).update(set__is_online=False):
+            updates.send_update(
+                views.HOME_CHANNEL_ID,
+                {
+                    'type': 'userlist',
+                    'action': 'PART',
+                    'username': user.username,
+                }
+            )
