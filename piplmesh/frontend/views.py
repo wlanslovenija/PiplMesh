bac4fff8e0aedc99ce71b1b0fcbc56ec56866d5c
Nejc Zupec <zupec.nejc@gmail.com>
2012-05-13 19:48:58 +0200

    Added function get_context_data().

--- piplmesh/frontend/views.py
+++ piplmesh/frontend/views.py
@@ -1,4 +1,3 @@
-from django import shortcuts
 from django.views import generic as generic_views
 
 from piplmesh.account import models
@@ -6,11 +5,10 @@ from piplmesh.account import models
 class HomeView(generic_views.TemplateView):
     template_name = 'home.html'
     
-    def get_logged_in_users(self, request):
-        users = models.User.objects(
-            is_online = True,
-        )
-        return shortcuts.render_to_response(self.template_name, {'logged_in_users': users}, context_instance=RequestContext(request))
-        
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['online_users'] = models.User.objects(is_online = True)
+        return context
+    
 class SearchView(generic_views.TemplateView):
     template_name = 'search.html'
