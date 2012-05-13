ac79f95d8ff3902aa32fff90bdbdcc837b0ac16a
Nejc Zupec <zupec.nejc@gmail.com>
2012-05-13 20:03:03 +0200

    Removed white spaces.

--- piplmesh/frontend/views.py
+++ piplmesh/frontend/views.py
@@ -7,7 +7,7 @@ class HomeView(generic_views.TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['online_users'] = models.User.objects(is_online = True)
+        context['online_users'] = models.User.objects(is_online=True)
         return context
     
 class SearchView(generic_views.TemplateView):
