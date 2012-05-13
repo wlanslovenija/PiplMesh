f10e9d15b09b67109830705b97dc14ea2bbfa435
premik91 <i@premik91.com>
2012-05-13 16:03:38 +0200

    Issue #141 - team 5
    A simple design for list of online users. I also added the toggle
    animation for hidding it and the title for this 'box' (Active Users or
    Logged in Users doesn't really sound good, but that's the least of my
    worries :)).
    
    Also added the ability to display only users that contain the searched
    string and to arrange list of logged in users by alphabet(design needs
    to be improved). Also the true image of the user should be passed in
    'data' to javascript. We (I and igypigi->did most of the javascript)
    were also unable to test this completely, because the updating logged
    in users doesn't always work(on our computers) and we weren't able to
    figure out why.

--- piplmesh/frontend/static/piplmesh/js/home.js
+++ piplmesh/frontend/static/piplmesh/js/home.js
@@ -1,13 +1,67 @@
-function updateUserlist(data) {
-	if (data.action == 'JOIN') {
-		$('<li/>').text(data.username).appendTo('#userlist');
-	}
-	else if (data.action == 'PART') {
-		// TODO: Improve escape and use more suitable :contains method
-		$('#userlist li:contains(' + escape(data.username) + ')').remove();
-	}
+var users = new Array();
+var searchUsers;
+var alphabeticalOrder = false;
+var userList;
+
+function updateUserList(data) {
+    if (data.action == 'JOIN') {
+        users[users.length] = data;
+        if (alphabeticalOrder) {
+            rebuildUserList();
+        } else {
+            addUserOnList(data);
+        }
+    }else if (data.action == 'PART') {
+        users.splice(users.indexOf(data), 1);
+        $("#userlist #" + data.id).remove();
+    }
+}
+
+// adds one user to UserList
+function addUserOnList(user) {
+    if (user.username.indexOf(searchUsers) != -1) {
+        userlist.append("<li id=\"" + user.id + "\">" +
+            "<img src=\"" + user.image + "\" alt=\"image\">" + user.username +
+            "<div class=\"userInfo\">" + user.info + "</div>" +
+            "</li>");
+    }
+}
+
+// rebuilds userList
+function rebuildUserList() {
+    var tmpusers = users;
+    userList.empty();
+    if (alphabeticalOrder) {
+        tmpusers.sort(function (user1, user2) {
+            if (user1.username.toUpperCase() < user2.username.toUpperCase()) return -1;
+            if (user1.username.toUpperCase() > user2.username.toUpperCase()) return 1;
+            return 0;
+        });
+    }
+    for (user in tmpusers) {
+        addUserOnList(user);
+    }
 }
 
 $(document).ready(function () {
-	$.updates.registerProcessor('home_channel', 'userlist', updateUserlist);
+    var userList = $("#userlist");
+    searchUsers = "";
+    $.updates.registerProcessor('home_channel', 'userlist', updateUserList);
+
+    $(".box .boxHeader").click(function () {
+        $(this).next("ul").slideToggle("slow");
+    });
+
+    $("#search_users").keyup(function () {
+        searchUsers = $(this).val();
+        rebuildUserList();
+    });
+
+    $("#alphabet_order").change(function () {
+        alphabeticalOrder = false;
+        if ($("#alphabet_order").is(":checked")) {
+            alphabeticalOrder = true;
+        }
+        rebuildUserList();
+    });
 });
--- piplmesh/frontend/tasks.py
+++ piplmesh/frontend/tasks.py
@@ -12,40 +12,38 @@ CHECK_ONLINE_USERS_RECONNECT_TIMEOUT = 2 * settings.CHECK_ONLINE_USERS_INTERVAL
 
 @task.task
 def check_online_users():
-    for user in models.User.objects(
-        is_online=False,
-        connections__ne=[],
-    ):
+    # TODO: Iterating over all users in Python could become really slow once there are millions of users, much better is to limit the query only to potentially interesting users (conditions are already bellow)
+    for user in models.User.objects():
         if models.User.objects(
-            pk=user.pk,
-            is_online=False,
-            connections__ne=[],
-        ).update(set__is_online=True):
-            updates.send_update(
-                views.HOME_CHANNEL_ID,
-                {
-                    'type': 'userlist',
-                    'action': 'JOIN',
-                    'username': user.username,
-                }
-            )
-
-    for user in models.User.objects(
-        is_online=True,
-        connections=[],
-        connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
-    ):
-        if models.User.objects(
-            pk=user.pk,
+            id=user.id,
             is_online=True,
             connections=[],
             connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
         ).update(set__is_online=False):
             updates.send_update(
                 views.HOME_CHANNEL_ID,
-                {
+                    {
                     'type': 'userlist',
                     'action': 'PART',
                     'username': user.username,
-                }
+                    # TODO get user image
+                    'image': '',
+                    'id': user.id,
+                    }
+            )
+        elif models.User.objects(
+            id=user.id,
+            is_online=False,
+            connections__ne=[],
+        ).update(set__is_online=True):
+            updates.send_update(
+                views.HOME_CHANNEL_ID,
+                    {
+                    'type': 'userlist',
+                    'action': 'JOIN',
+                    'username': user.username,
+                    # TODO get user image
+                    'image': '',
+                    'id': user.id,
+                    }
             )
