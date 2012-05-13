0b62cf5a9832d570e645429fbc37211a030929a9
premik91 <i@premik91.com>
2012-05-13 16:09:18 +0200

    Fixing tasks.py

--- piplmesh/frontend/tasks.py
+++ piplmesh/frontend/tasks.py
@@ -12,10 +12,34 @@ CHECK_ONLINE_USERS_RECONNECT_TIMEOUT = 2 * settings.CHECK_ONLINE_USERS_INTERVAL
 
 @task.task
 def check_online_users():
-    # TODO: Iterating over all users in Python could become really slow once there are millions of users, much better is to limit the query only to potentially interesting users (conditions are already bellow)
-    for user in models.User.objects():
+    for user in models.User.objects(
+        is_online=False,
+        connections__ne=[],
+    ):
         if models.User.objects(
-            id=user.id,
+            pk=user.pk,
+            is_online=False,
+            connections__ne=[],
+        ).update(set__is_online=True):
+            updates.send_update(
+                views.HOME_CHANNEL_ID,
+                    {
+                    'type': 'userlist',
+                    'action': 'JOIN',
+                    'username': user.username,
+                    # TODO get user image
+                    'image': '',
+                    'id': user.id,
+                    }
+            )
+
+    for user in models.User.objects(
+        is_online=True,
+        connections=[],
+        connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
+    ):
+        if models.User.objects(
+            pk=user.pk,
             is_online=True,
             connections=[],
             connection_last_unsubscribe__lt=datetime.datetime.now() - datetime.timedelta(seconds=CHECK_ONLINE_USERS_RECONNECT_TIMEOUT),
@@ -25,22 +49,6 @@ def check_online_users():
                     {
                     'type': 'userlist',
                     'action': 'PART',
-                    'username': user.username,
-                    # TODO get user image
-                    'image': '',
-                    'id': user.id,
-                    }
-            )
-        elif models.User.objects(
-            id=user.id,
-            is_online=False,
-            connections__ne=[],
-        ).update(set__is_online=True):
-            updates.send_update(
-                views.HOME_CHANNEL_ID,
-                    {
-                    'type': 'userlist',
-                    'action': 'JOIN',
                     'username': user.username,
                     # TODO get user image
                     'image': '',
