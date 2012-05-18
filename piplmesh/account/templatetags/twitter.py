59314fb3ae50c343c416064dd167649664946ca5
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-05-18 16:35:39 +0200

    Added twitter_picture to twitter template tag

---
+++ piplmesh/account/templatetags/twitter.py
@@ -0,0 +1,11 @@
+import tweepy
+
+from django import template
+from django.conf import settings
+register = template.Library()
+
+@register.simple_tag
+def twitter_picture(key, secret):
+    twitter_auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+    twitter_auth.set_access_token(key, secret)
+    return tweepy.API(twitter_auth).me().profile_image_url
