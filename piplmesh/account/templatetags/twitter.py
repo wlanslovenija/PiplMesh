b637ba5d498c6a83191d172c4b5bf49ad45a2ace
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-05-20 19:31:51 +0200

    Variables names edited

--- piplmesh/account/templatetags/twitter.py
+++ piplmesh/account/templatetags/twitter.py
@@ -6,7 +6,7 @@ import tweepy
 register = template.Library()
 
 @register.simple_tag
-def twitter_picture(key, secret):
+def twitter_picture(token_key, token_secret):
     twitter_auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
-    twitter_auth.set_access_token(key, secret)
+    twitter_auth.set_access_token(token_key, token_secret)
     return tweepy.API(twitter_auth).me().profile_image_url
