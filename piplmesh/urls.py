02035404c0f090d02334a3d9555e084b0466ccd0
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-05-20 17:08:38 +0200

    Removing empty line.

--- piplmesh/urls.py
+++ piplmesh/urls.py
@@ -27,5 +27,4 @@ urlpatterns = patterns('',
     url(r'^user/(?P<username>' + models.USERNAME_REGEX + ')/$', frontend_views.UserView.as_view(), name='user'),
     url(r'^account/$', account_views.AccountChangeView.as_view(), name='account'),
     url(r'^account/password/change/$', account_views.PasswordChangeView.as_view(), name='password_change'),
-
 )
