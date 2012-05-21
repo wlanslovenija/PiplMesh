138f71f559702eff7be0c5afbf191f32968358e1
premik91 <i@premik91.com>
2012-05-21 13:45:01 +0200

    jq minor fixes

--- piplmesh/frontend/static/piplmesh/js/home.js
+++ piplmesh/frontend/static/piplmesh/js/home.js
@@ -3,7 +3,7 @@ var onlineUsers = {
         {{ user.username }}: {{ user }} {% if not forloop.last %}, {% endif %}
     {% endfor %}
 };
-var searchUsers = "";
+var searchUsers = '';
 
 function User(data) {
     var self = this;
@@ -22,18 +22,18 @@ function redrawUserList() {
         if (key1.toUpperCase() > key2.toUpperCase()) return 1;
         return 0;
     });
-    $("#userlist").empty();
+    $('#userlist').empty();
     for (key in keys) {
-        if (searchUsers === "" || key.indexOf(searchUsers) != -1) {
+        if (searchUsers === '' || key.indexOf(searchUsers) != -1) {
             user = onlineUsers[key];
             var li = $('<li/>');
-            var image = $('<img/>').attrs({'src':user.image_url, 'alt':'User image'});
+            var image = $('<img/>').attrs({'src': user.image_url, 'alt': 'User image'});
             li.append(image);
             li.append(user.username);
-            var div = $('<div/>').attrs({'class':'info'});
+            var div = $('<div/>').attrs({'class': 'info'});
             div.append(user.info);
             li.append(div);
-            $("#userlist").append(li);
+            $('#userlist').append(li);
         }
     }
 }
@@ -41,13 +41,11 @@ function redrawUserList() {
 function updateUserList(data) {
     var user = new User(data.user);
     if (data.action === 'JOIN') {
-        if (!onlineUsers.hasOwnProperty(user.username)) {
-            onlineUsers[user.username] = user;
-            redrawUserList();
-        }
+        onlineUsers[user.username] = user;
+        redrawUserList();
     } else if (data.action === 'PART') {
         var index = onlineUsers.indexOf(data);
-        if (onlineUsers.hasOwnProperty(user.username)) {
+        if (onlineUsers[user.username]) {
             delete onlineUsers[user.username];
             redrawUserList();
         }
@@ -57,11 +55,11 @@ function updateUserList(data) {
 $(document).ready(function () {
     $.updates.registerProcessor('home_channel', 'userlist', updateUserList);
 
-    $(".panel .header").click(function () {
-        $(this).next("ul").slideToggle("slow");
+    $('.panel .header').click(function () {
+        $(this).next('ul').slideToggle('slow');
     });
 
-    $("#search_users").keyup(function () {
+    $('#search_users').change(function () {
         searchUsers = $(this).val();
         redrawUserList();
     });
