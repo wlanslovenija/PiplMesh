d9d4c2896293fe4e87c16a9b38332fb59b6ec58c
Aljana <aljana@gmail.com>
2012-05-24 22:16:53 +0200

    Created a form for posting new posts, added a bit javascript and a jquery library that nicely resize form. I Also created a basic template for listing posts - html content is only temporary and it will be replaced with javascript generated content

--- piplmesh/frontend/static/piplmesh/js/home.js
+++ piplmesh/frontend/static/piplmesh/js/home.js
@@ -1,15 +1,15 @@
 function updateUserlist(data) {
-	if (data.action == 'JOIN') {
-		$('<li/>').text(data.username).appendTo('#userlist');
-	}
-	else if (data.action == 'PART') {
-		// TODO: Improve escape and use more suitable :contains method
-		$('#userlist li:contains(' + escape(data.username) + ')').remove();
-	}
+    if (data.action == 'JOIN') {
+        $('<li/>').text(data.username).appendTo('#userlist');
+    }
+    else if (data.action == 'PART') {
+        // TODO: Improve escape and use more suitable :contains method
+        $('#userlist li:contains(' + escape(data.username) + ')').remove();
+    }
 }
 
 $(document).ready(function () {
-	$.updates.registerProcessor('home_channel', 'userlist', updateUserlist);
+    $.updates.registerProcessor('home_channel', 'userlist', updateUserlist);
 
     $.getJSON('/api/v1/post/',function(data){
         console.log(data);
@@ -19,4 +19,17 @@ $(document).ready(function () {
             }
         });
     });
+    
+    // TODO: post new msg
+    $('#submit_post').click(function () { 
+        alert('TODO'); 
+    });
+    
+    $('#post_text').expandingTextArea();
+    
+    $('#post_text').click(function () {
+        $('#post_text').html('');
+        $('#post_text').css({'min-height':50});
+    });
+
 });
