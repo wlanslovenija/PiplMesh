473737926142343232808b107f8feb0287ce7b58
Miha Novak <m.miha.novak@gmail.com>
2012-05-30 10:53:39 +0200

    Added description for foursquare_user fields.

--- piplmesh/account/backends.py
+++ piplmesh/account/backends.py
@@ -126,6 +126,20 @@ class FoursquareBackend(MongoEngineBackend):
         data = urllib.urlopen('https://api.foursquare.com/v2/users/self?oauth_token=%s' % access_token)
         foursquare_data = json.load(data)
         foursquare_user = foursquare_data['response']['user']
+        """
+        Fields in foursquare_user:
+        id - A unique identifier for this user.
+        firstName - A user's first name.
+        lastName - A user's last name.
+        homeCity - User's home city.
+        photo - URL of a profile picture for this user.
+        gender - A user's gender: male, female, or none.
+        relationship - (Optional) The relationship of the acting user (me) to this user (them).
+
+        Present in user details:
+        type, contact, pings, badges, checkins, mayorships, tips, todos, photos, friends, followers, 
+        requests, pageInfo.
+        """
 
         user, created = self.user_class.objects.get_or_create(
             foursquare_id=foursquare_user.get('id'),
