8d948ed4360f7107a21b5682ede2ccb872666f32
agiz <ziga.zupanec@gmail.com>
2012-06-05 10:13:02 +0200

    Added logic to display node info.

--- piplmesh/frontend/static/piplmesh/js/map.js
+++ piplmesh/frontend/static/piplmesh/js/map.js
@@ -14,31 +14,17 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-    var nodeDiv = $('<div/>').prop({
-        'class': 'node_content'
-    });
-    var nodeName = $('<h3/>').text(node.name);
-    var nodeContent = $('<div/>').prop({
-        'id': 'bodyContent'
-    });
-    var nodeAddress = $('<p/>').text(gettext("Location:")+' '+node.location);
-    var nodeWebsiteText = $('<p/>').text(gettext("Website:"));
-    var nodeWebsite = $('<a/>').prop('href', node.url).text(node.url);
-    nodeDiv.append(nodeName);
-    nodeContent.append(nodeAddress);
-    nodeWebsiteText.append(nodeWebsite);
-    nodeContent.append(nodeWebsiteText);
-    nodeDiv.append(nodeContent);
-    var infowindow = new google.maps.InfoWindow({
-            content: nodeDiv.html()
-    });
+    var nodeName = $('<p/>').text(node.name+' | ');
+    var nodeWebsite = $('<a/>').prop('href', node.url).text(gettext("more info"));
+	nodeName.append(nodeWebsite);
+    $('#map_info').append(nodeName).hide();
     var marker = new google.maps.Marker({
             position: nodeLocation,
             map: map,
             title: node.name
     });
     google.maps.event.addListener(marker, 'click', function (event) {
-            infowindow.open(map, marker);
+            $('#map_info').toggle('fast');
         }
     );
 }
