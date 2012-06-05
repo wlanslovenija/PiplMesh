e3aec1366c3147b849e8956da36e28be06e11c81
agiz <ziga.zupanec@gmail.com>
2012-06-06 00:25:39 +0200

    Re=arranged code, removed toggle effect.

--- piplmesh/frontend/static/piplmesh/js/map.js
+++ piplmesh/frontend/static/piplmesh/js/map.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    initialize();
-});
-
-function initialize() {
     var nodeLocation = new google.maps.LatLng(node.latitude, node.longitude);
     var myOptions = {
         zoom: 15,
@@ -11,20 +7,17 @@ function initialize() {
         navigationControl: false,
         scaleControl: false,
         draggable: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: false
+    };
     var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-    var nodeName = $('<p/>').text(node.name+' | ');
+    var nodeName = $('<p/>').text(node.name).append(' | ');
     var nodeWebsite = $('<a/>').prop('href', node.url).text(gettext("more info"));
     nodeName.append(nodeWebsite);
-    $('#map_info').append(nodeName).hide();
+    $('#map_info').append(nodeName);
     var marker = new google.maps.Marker({
-            position: nodeLocation,
-            map: map,
-            title: node.name
+        position: nodeLocation,
+        map: map,
+        title: node.name
     });
-    google.maps.event.addListener(marker, 'click', function (event) {
-            $('#map_info').toggle('fast');
-        }
-    );
-}
+});
