b16c08fd3be7689ab884604b8189ce694b6d3846
Matevž <matevz.mihalic@gmail.com>
2012-06-09 17:45:11 +0200

    Simplified filter in PostAuthorization.

--- piplmesh/api/authorization.py
+++ piplmesh/api/authorization.py
@@ -4,7 +4,7 @@ from tastypie import authorization as tastypie_authorization
 class PostAuthorization(tastypie_authorization.Authorization):
     def apply_limits(self, request, object_list):
         if request and hasattr(request, 'user'):
-            object_list = object_list.filter((queryset.Q(author=request.user) & queryset.Q(is_published=False)) | queryset.Q(is_published=True))
+            object_list = object_list.filter(queryset.Q(author=request.user) | queryset.Q(is_published=True))
         else:
             object_list = object_list.filter(is_published=True)
 
