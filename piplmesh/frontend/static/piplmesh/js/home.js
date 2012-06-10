df57656fbbee169e010a74df544f7733ae88b49c
Nejc Zupec <zupec.nejc@gmail.com>
2012-06-10 23:53:32 +0200

    Added JS code, which renders columns for panels.

--- piplmesh/frontend/static/piplmesh/js/home.js
+++ piplmesh/frontend/static/piplmesh/js/home.js
@@ -1,4 +1,24 @@
+function howManyColumns() {
+    var panelsWidth = $('#panels').width();
+    var columnPanelsWidth = $('.panels_column').width() + parseInt($('.panels_column').css('margin-left'));
+
+    return parseInt(panelsWidth / columnPanelsWidth);
+}
+
+function fillWithColumns() {
+    var currentColumns = $('#panels').children().length;
+
+    for(i = currentColumns; i < howManyColumns(); i++) {
+        $('#panels').append('<div class="panels_column"></div>');
+    }
+}
+
 $(document).ready(function () {
+
+
+    fillWithColumns();
+    
+    
     $('.panel .header').click(function (event) {
         $(this).next('.content').slideToggle('fast');
     });
