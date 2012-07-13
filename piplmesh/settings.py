24679dc9d12e221c54b91fb31ecd13901eb40e3d
BorisMarn <boris.marn@gmail.com>
2012-07-13 18:41:30 +0200

    pravverzija rahlo spremenjenega CSSa. Nekak morm pogruntat kako vse to lepo narest.

--- piplmesh/settings.py
+++ piplmesh/settings.py
@@ -161,6 +161,7 @@ ROOT_URLCONF = 'piplmesh.urls'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 TEMPLATE_DIRS = (
+    'piplmesh/account/templates/user/'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
