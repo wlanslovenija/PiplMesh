5af16912f2189b2c4cfd42c8ccc2c27c4c5cdafe
Nejc Zupec <zupec.nejc@gmail.com>
2012-08-23 16:49:36 +0200

    Removed dummy panels.

--- piplmesh/panels/dummy/panel.py
+++ piplmesh/panels/dummy/panel.py
@@ -15,35 +15,5 @@ class DummyPanel(panels.BasePanel):
         })
         return context
 
-class DummyPanel2(panels.BasePanel):
-    def get_context(self, context):
-        context.update({
-            'header': _("Dummy panel 2"),
-            'content': '\n\n'.join(lorem_ipsum.paragraphs(random.randint(1, 1))),
-            'id': 'dummy2',
-        })
-        return context
-
-class DummyPanel3(panels.BasePanel):
-    def get_context(self, context):
-        context.update({
-            'header': _("Dummy panel 3"),
-            'content': '\n\n'.join(lorem_ipsum.paragraphs(random.randint(1, 1))),
-            'id': 'dummy3',
-        })
-        return context
-
-class DummyPanel4(panels.BasePanel):
-    def get_context(self, context):
-        context.update({
-            'header': _("Dummy panel 4"),
-            'content': '\n\n'.join(lorem_ipsum.paragraphs(random.randint(1, 1))),
-            'id': 'dummy4',
-        })
-        return context
-
 if settings.DEBUG:
     panels.panels_pool.register(DummyPanel)
-    panels.panels_pool.register(DummyPanel2)
-    panels.panels_pool.register(DummyPanel3)
-    panels.panels_pool.register(DummyPanel4)
