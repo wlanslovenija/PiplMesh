2bc455b1ada78e1ca79a7265610296ec2ea187b6
Nejc Zupec <zupec.nejc@gmail.com>
2012-08-24 15:21:15 +0200

    Added jQuery CSS file and images. Added common.js. Small code fixes.

--- piplmesh/frontend/static/piplmesh/js/home.js
+++ piplmesh/frontend/static/piplmesh/js/home.js
@@ -24,7 +24,7 @@ function resetColumns() {
     });
 }
 
-function fillWithColumns() {
+function initializeEmptyColumnsForPanels() {
     var currentColumns = $('#panels').children().length;
     var noOfColumns = howManyColumns();
 
@@ -85,7 +85,7 @@ function collapsePanels() {
 }
 
 function preparePanels() {
-    fillWithColumns();
+    initializeEmptyColumnsForPanels();
     orderPanels();
     collapsePanels();
     makeColumnsSortable();
@@ -115,48 +115,15 @@ $(document).ready(function () {
     $('.panel .header').click(function (event) {
         var visible = $(this).next().is(':visible');
         $(this).next('.content').slideToggle('fast');
-        $.post(urls['panels_collapse'], 'data=' + JSON.stringify( {panel_id: $(this).parent().attr('id'), collapsed: (visible) ? true : false }));
+
+        var panel_id = $(this).parent().attr('id');
+        var collapsed =  visible ? true : false;
+
+        $.post(urls['panels_collapse'], 'data=' + JSON.stringify( {panel_id: panel_id, collapsed: collapsed }));
     });
 
     $(window).resize(function () {
         resetColumns();
         preparePanels();
     });
-});
-
-$(document).ajaxSend(function(event, xhr, settings) {
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    function sameOrigin(url) {
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-    function safeMethod(method) {
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-
-    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    }
 });
