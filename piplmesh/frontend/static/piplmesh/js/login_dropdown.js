46a45159ef97ff91063b02eea5d14609afa2c785
igypigi <igypigi@gmail.com>
2012-08-27 03:21:28 +0200

    Added login drop-down menu

---
+++ piplmesh/frontend/static/piplmesh/js/login_dropdown.js
@@ -0,0 +1,9 @@
+$('#drop_down_login_options').hide();
+$('#drop_down_login_container').hover(
+    function () {
+        $('#drop_down_login_options').show();
+    },
+    function () {
+        $('#drop_down_login_options').hide();
+    }
+)
