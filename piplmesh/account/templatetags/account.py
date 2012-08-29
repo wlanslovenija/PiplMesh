aee91945da024465cb3153cce8089cf9777a964f
BorisMarn <boris.marn@gmail.com>
2012-08-29 23:01:31 +0200

    moved user account/profile menu to its own template file

--- piplmesh/account/templatetags/account.py
+++ piplmesh/account/templatetags/account.py
@@ -10,3 +10,10 @@ def user_image(context, user=None):
     return {
         'user_image_url': user.get_image_url(),
     }
+
+@register.simple_tag
+def active(request, pattern):
+    import re
+    if re.search(pattern, request.path):
+        return 'current_item'
+    return ''
