a30506244ffa8ab7e9ab9e4d2722a7cd7659b8c1
Mitar <mitar.git@tnode.com>
2012-09-07 04:03:36 -0700

    Should use timezone-aware dates.

--- piplmesh/account/models.py
+++ piplmesh/account/models.py
@@ -22,10 +22,10 @@ USERNAME_REGEX = r'[\w.@+-]+'
 CONFIRMATION_TOKEN_VALIDITY = 5 # days
 
 def upper_birthdate_limit():
-    return datetime.datetime.today()
+    return timezone.now().date()
 
 def lower_birthdate_limit():
-    return datetime.datetime.today() - datetime.timedelta(LOWER_DATE_LIMIT)
+    return timezone.now().date() - datetime.timedelta(LOWER_DATE_LIMIT)
 
 class Connection(mongoengine.EmbeddedDocument):
     http_if_none_match = mongoengine.StringField()
