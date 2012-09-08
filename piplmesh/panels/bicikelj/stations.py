424d6a241b070230514245264a66373cc0808ee1
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-08 12:36:00 +0200

    Functions for bicikelj stations

---
+++ piplmesh/panels/bicikelj/stations.py
@@ -0,0 +1,49 @@
+import math, urllib2, datetime
+from xml.dom import minidom
+
+from . import models
+
+BICIKELJ_STATIONS_URL = 'http://www.bicikelj.si/service/carto'
+BICIKELJ_INFO_URL = 'http://www.bicikelj.si/service/stationdetails/ljubljana/'
+BICIKELJ_BOUNDS = 0.00965
+ZOOM_LEVEL = 15
+MAP_WIDTH = 450
+C = 0.703119412486786 # constant at 60 degrees
+
+def max_distance(zoom, width):
+    return C*width/math.pow(2,zoom)
+
+def get_stations_nearby(lat, lng):
+    maxd = max_distance(ZOOM_LEVEL,MAP_WIDTH)
+    ids = []
+    while 1:
+        try:
+            ids.append(models.BicikeljStation.objects(location__near= [lat, lng], location__within_box=[(lat-abs(maxd),lng-abs(maxd)),(lat+abs(maxd),lng+abs(maxd))], id__nin=ids).first().id)
+        except:
+            break
+    stations = [models.BicikeljStation.objects(id=n).order_by('-timestamp').first() for n in ids]
+    return stations
+
+def fetch_data():
+    stations_data = urllib2.urlopen(BICIKELJ_STATIONS_URL).read()
+    stations_xml_data = minidom.parseString(stations_data)
+    stations_nodes = stations_xml_data.getElementsByTagName('marker')
+    print stations_nodes
+    info_data = [urllib2.urlopen(BICIKELJ_INFO_URL+str(number)).read() for number in range(1,len(stations_nodes)+1)]
+    info_xml_data = [minidom.parseString(info_data[i]) for i in range(len(stations_nodes))]
+    data = [
+        {
+        'name': stations_nodes[number].attributes['name'].value,
+        'address': stations_nodes[number].attributes['address'].value,
+        'id': int(stations_nodes[number].attributes['number'].value),
+        'location': [float(stations_nodes[number].attributes['lat'].value), float(stations_nodes[number-1].attributes['lng'].value)],
+        'available': int(info_xml_data[number].getElementsByTagName('available')[0].firstChild.nodeValue),
+        'free': int(info_xml_data[number].getElementsByTagName('free')[0].firstChild.nodeValue),
+        'total': int(info_xml_data[number].getElementsByTagName('total')[0].firstChild.nodeValue),
+        'open': int(info_xml_data[number].getElementsByTagName('open')[0].firstChild.nodeValue),
+        'timestamp': datetime.datetime.fromtimestamp(int(info_xml_data[number].getElementsByTagName('updated')[0].firstChild.nodeValue)),
+        }
+        for number in range(len(stations_nodes))]
+    for d in data:
+        print d['timestamp']
+    return data
