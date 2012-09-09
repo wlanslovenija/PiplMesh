9f12c5392b908e9f7c361370cf2ab33d325548fd
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-09 12:59:04 +0200

    Indexes etc

--- piplmesh/panels/bicikelj/models.py
+++ piplmesh/panels/bicikelj/models.py
@@ -1,12 +1,16 @@
 import mongoengine
 
 class BicikeljStation(mongoengine.Document):
-    id = mongoengine.IntField(required=True)
+    station_id = mongoengine.IntField(required=True, unique_with='timestamp')
     name = mongoengine.StringField(required=True)
     address = mongoengine.StringField(required=True)
     location = mongoengine.GeoPointField(required=True)
-    open = mongoengine.IntField()
-    available = mongoengine.IntField()
-    free = mongoengine.IntField()
-    total = mongoengine.IntField()
-    timestamp = mongoengine.DateTimeField()
+    open = mongoengine.BooleanField(required=True)
+    available = mongoengine.IntField(required=True)
+    free = mongoengine.IntField(required=True)
+    total = mongoengine.IntField(required=True)
+    timestamp = mongoengine.DateTimeField(required=True, unique_with='station_id')
+
+    meta = {
+        'indexes': ['station_id', 'timestamp'],
+    }
