fa9c6f4f3d147e179ba265c43f154ad807c5df98
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-09 12:59:39 +0200

    Fixes here and there

--- piplmesh/panels/bicikelj/stations.py
+++ piplmesh/panels/bicikelj/stations.py
@@ -1,17 +1,22 @@
-import math, urllib2, datetime
+import datetime, urllib2
 from xml.dom import minidom
 
 from . import models
 
 BICIKELJ_STATIONS_URL = 'http://www.bicikelj.si/service/carto'
-BICIKELJ_INFO_URL = 'http://www.bicikelj.si/service/stationdetails/ljubljana/'
-BICIKELJ_BOUNDS = 0.00965
+BICIKELJ_INFO_URL = 'http://www.bicikelj.si/service/stationdetails/ljubljana/%d'
+"""
+BOUNDS_LATITUDE = map_heigth*0.703119412486786/2^(map_zoom+1)
+BOUNDS_LONGITUDE = map_width*0.703119412486786/2^(map_zoom+1)
+"""
+BICIKELJ_BOUNDS_LATITUDE = 0.0027895
+BICIKELJ_BOUNDS_LONGITUDE = 0.0027895
 
-def get_stations_nearby(lat, lng):
+def get_stations_nearby(latitude, longitude):
     ids = []
     while 1:
         try:
-            ids.append(models.BicikeljStation.objects(location__near= [lat, lng], location__within_box=[(lat-BICIKELJ_BOUNDS,lng-BICIKELJ_BOUNDS),(lat+BICIKELJ_BOUNDS,lng+BICIKELJ_BOUNDS)], id__nin=ids).first().id)
+            ids.append(models.BicikeljStation.objects(location__near= (latitude, longitude), location__within_box=((latitude-BICIKELJ_BOUNDS_LATITUDE,longitude-BICIKELJ_BOUNDS_LONGITUDE),(latitude+BICIKELJ_BOUNDS_LATITUDE,longitude+BICIKELJ_BOUNDS_LONGITUDE)), id__nin=ids).first().id)
         except:
             break
     stations = [models.BicikeljStation.objects(id=n).order_by('-timestamp').first() for n in ids]
@@ -21,21 +26,21 @@ def fetch_data():
     stations_data = urllib2.urlopen(BICIKELJ_STATIONS_URL).read()
     stations_xml_data = minidom.parseString(stations_data)
     stations_nodes = stations_xml_data.getElementsByTagName('marker')
-    info_data = [urllib2.urlopen(BICIKELJ_INFO_URL+str(number)).read() for number in range(1,len(stations_nodes)+1)]
+    info_data = [urllib2.urlopen(BICIKELJ_INFO_URL % number).read() for number in range(1,len(stations_nodes)+1)]
     info_xml_data = [minidom.parseString(info_data[i]) for i in range(len(stations_nodes))]
     data = [
+        [{
+            'station_id': int(stations_nodes[number].attributes['number'].value),
+            'timestamp': datetime.datetime.fromtimestamp(int(info_xml_data[number].getElementsByTagName('updated')[0].firstChild.nodeValue)),
+        },
         {
-        'name': stations_nodes[number].attributes['name'].value,
-        'address': stations_nodes[number].attributes['address'].value,
-        'id': int(stations_nodes[number].attributes['number'].value),
-        'location': [float(stations_nodes[number].attributes['lat'].value), float(stations_nodes[number-1].attributes['lng'].value)],
-        'available': int(info_xml_data[number].getElementsByTagName('available')[0].firstChild.nodeValue),
-        'free': int(info_xml_data[number].getElementsByTagName('free')[0].firstChild.nodeValue),
-        'total': int(info_xml_data[number].getElementsByTagName('total')[0].firstChild.nodeValue),
-        'open': int(info_xml_data[number].getElementsByTagName('open')[0].firstChild.nodeValue),
-        'timestamp': datetime.datetime.fromtimestamp(int(info_xml_data[number].getElementsByTagName('updated')[0].firstChild.nodeValue)),
-        }
+            'name': stations_nodes[number].attributes['name'].value,
+            'address': stations_nodes[number].attributes['address'].value,
+            'location': [float(stations_nodes[number].attributes['lat'].value), float(stations_nodes[number-1].attributes['lng'].value)],
+            'available': int(info_xml_data[number].getElementsByTagName('available')[0].firstChild.nodeValue),
+            'free': int(info_xml_data[number].getElementsByTagName('free')[0].firstChild.nodeValue),
+            'total': int(info_xml_data[number].getElementsByTagName('total')[0].firstChild.nodeValue),
+            'open': int(info_xml_data[number].getElementsByTagName('open')[0].firstChild.nodeValue),
+        }]
         for number in range(len(stations_nodes))]
-    for d in data:
-        print d['timestamp']
     return data
