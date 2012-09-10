cc62f7fb28e1e8e2ef807ff6674737f194b9ccde
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-10 23:07:58 +0200

    Simplified get_or_create query

--- piplmesh/panels/bicikelj/tasks.py
+++ piplmesh/panels/bicikelj/tasks.py
@@ -7,15 +7,10 @@ from . import models, stations
 
 @task.task
 def update_station_info():
-    stations_data = stations.fetch_data()
-    for station in stations_data:
+    for station in stations.fetch_data():
         station_object, created = models.BicikeljStation.objects.get_or_create(
-            station_id=station[0]['station_id'],
-            timestamp=station[0]['timestamp'],
-            defaults = station[1]
+            station_id=station['station_id'],
+            timestamp=station['timestamp'],
+            defaults=station,
         )
-        if created:
-            updates.send_update(
-                views.HOME_CHANNEL_ID,
-                # TODO
-            )
+        # TODO: if created, send an update to server
