25334afdd0543c14d780f58c7c40ab2229446dd9
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-10 23:07:11 +0200

    Fixing and cleaning

--- piplmesh/panels/bicikelj/stations.py
+++ piplmesh/panels/bicikelj/stations.py
@@ -1,46 +1,62 @@
-import datetime, urllib2
-from xml.dom import minidom
+import urllib2
+from datetime import date, datetime, timedelta
+from lxml import etree, objectify
+from pytz import UTC
 
+from piplmesh import settings
 from . import models
 
 BICIKELJ_STATIONS_URL = 'http://www.bicikelj.si/service/carto'
 BICIKELJ_INFO_URL = 'http://www.bicikelj.si/service/stationdetails/ljubljana/%d'
+
+""" A comment explaining how to calculate map bounds, so we can search for stations visible on map only
+
+BOUNDS_LATITUDE and BOUNDS_LONGITUDE are the distance between a node and top location of the map
+and the distance between a node and leftmost location of the map, respectively
+BOUNDS_LATITUDE = map_heigth*0.703119412486786/2^map_zoom
+BOUNDS_LONGITUDE = map_width*0.703119412486786/2^map_zoom
+
 """
-BOUNDS_LATITUDE = map_heigth*0.703119412486786/2^(map_zoom+1)
-BOUNDS_LONGITUDE = map_width*0.703119412486786/2^(map_zoom+1)
-"""
-BICIKELJ_BOUNDS_LATITUDE = 0.0027895
-BICIKELJ_BOUNDS_LONGITUDE = 0.0027895
+
+BICIKELJ_BOUNDS_LATITUDE = 0.00557895
+BICIKELJ_BOUNDS_LONGITUDE = 0.00557895
 
 def get_stations_nearby(latitude, longitude):
-    ids = []
-    while 1:
-        try:
-            ids.append(models.BicikeljStation.objects(location__near= (latitude, longitude), location__within_box=((latitude-BICIKELJ_BOUNDS_LATITUDE,longitude-BICIKELJ_BOUNDS_LONGITUDE),(latitude+BICIKELJ_BOUNDS_LATITUDE,longitude+BICIKELJ_BOUNDS_LONGITUDE)), id__nin=ids).first().id)
-        except:
-            break
-    stations = [models.BicikeljStation.objects(id=n).order_by('-timestamp').first() for n in ids]
-    return stations
+    stations_nearby_all = models.BicikeljStation.objects(
+                location__near=(latitude, longitude),
+                location__within_box=((latitude-BICIKELJ_BOUNDS_LATITUDE,longitude-BICIKELJ_BOUNDS_LONGITUDE),(latitude+BICIKELJ_BOUNDS_LATITUDE,longitude+BICIKELJ_BOUNDS_LONGITUDE)),
+                fetch_time__gt=datetime.now() - timedelta(seconds=12*settings.POLL_BICIKELJ_INTERVAL),
+    )
+    n = stations_nearby_all[0].station_id
+    station_idx = []
+    for j,station in enumerate(stations_nearby_all):
+        if station.station_id != n:
+            station_idx.append(j-1)
+            n = station.station_id
+    station_idx.append(len(stations_nearby_all)-1)
+    stations_nearby = [stations_nearby_all[i] for i in station_idx]
+    for station in stations_nearby:
+        station.old_data = station.fetch_time < UTC.localize(datetime.now() - timedelta(seconds=2*settings.POLL_BICIKELJ_INTERVAL))
+        station.very_old_data = station.fetch_time < UTC.localize(datetime.now() - timedelta(seconds=10*settings.POLL_BICIKELJ_INTERVAL))
+    return stations_nearby
 
 def fetch_data():
-    stations_data = urllib2.urlopen(BICIKELJ_STATIONS_URL).read()
-    stations_xml_data = minidom.parseString(stations_data)
-    stations_nodes = stations_xml_data.getElementsByTagName('marker')
-    info_data = [urllib2.urlopen(BICIKELJ_INFO_URL % number).read() for number in range(1,len(stations_nodes)+1)]
-    info_xml_data = [minidom.parseString(info_data[i]) for i in range(len(stations_nodes))]
-    data = [
-        [{
-            'station_id': int(stations_nodes[number].attributes['number'].value),
-            'timestamp': datetime.datetime.fromtimestamp(int(info_xml_data[number].getElementsByTagName('updated')[0].firstChild.nodeValue)),
-        },
-        {
-            'name': stations_nodes[number].attributes['name'].value,
-            'address': stations_nodes[number].attributes['address'].value,
-            'location': [float(stations_nodes[number].attributes['lat'].value), float(stations_nodes[number-1].attributes['lng'].value)],
-            'available': int(info_xml_data[number].getElementsByTagName('available')[0].firstChild.nodeValue),
-            'free': int(info_xml_data[number].getElementsByTagName('free')[0].firstChild.nodeValue),
-            'total': int(info_xml_data[number].getElementsByTagName('total')[0].firstChild.nodeValue),
-            'open': int(info_xml_data[number].getElementsByTagName('open')[0].firstChild.nodeValue),
-        }]
-        for number in range(len(stations_nodes))]
+    stations_tree = objectify.fromstring(urllib2.urlopen(BICIKELJ_STATIONS_URL).read())
+    data = []
+    for node in stations_tree.markers.marker:
+        info_data = objectify.fromstring(urllib2.urlopen(BICIKELJ_INFO_URL % int(node.attrib['number'])).read())
+        data.append(
+            {
+                'station_id': int(node.attrib['number']),
+                'timestamp': datetime.fromtimestamp(int(info_data.updated)),
+                'name': node.attrib['name'],
+                'address': node.attrib['address'],
+                'location': (float(node.attrib['lat']), float(node.attrib['lng'])),
+                'available': int(info_data.available),
+                'free': int(info_data.free),
+                'total': int(info_data.total),
+                'open': bool(info_data.open),
+                'fetch_time': datetime.now(),
+            }
+        )
     return data
