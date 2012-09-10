7df0d8ce9160a71fc30d2c46e05ecd71f310a157
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-10 23:06:26 +0200

    Added fetched_time field

--- piplmesh/panels/bicikelj/models.py
+++ piplmesh/panels/bicikelj/models.py
@@ -2,6 +2,8 @@ import mongoengine
 
 class BicikeljStation(mongoengine.Document):
     station_id = mongoengine.IntField(required=True, unique_with='timestamp')
+    timestamp = mongoengine.DateTimeField(required=True, unique_with='station_id')
+    fetch_time = mongoengine.DateTimeField(required=True)
     name = mongoengine.StringField(required=True)
     address = mongoengine.StringField(required=True)
     location = mongoengine.GeoPointField(required=True)
@@ -9,7 +11,6 @@ class BicikeljStation(mongoengine.Document):
     available = mongoengine.IntField(required=True)
     free = mongoengine.IntField(required=True)
     total = mongoengine.IntField(required=True)
-    timestamp = mongoengine.DateTimeField(required=True, unique_with='station_id')
 
     meta = {
         'indexes': ['station_id', 'timestamp'],
