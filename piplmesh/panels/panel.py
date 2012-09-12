e669ff46c930e8359ca5beea5fc76f268bcdea90
Mitar <mitar.git@tnode.com>
2012-09-11 20:12:01 -0700

    Load panels' tasks.

--- piplmesh/panels/panel.py
+++ piplmesh/panels/panel.py
@@ -5,7 +5,7 @@ from django.utils import importlib
 # A fake panel module which loads all bundled panels instead
 
 for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]):
-    if module in ('models', 'panel'):
+    if module in ('models', 'panel', 'tasks'):
         # Prevent failing because of circular imports (models imports panel)
         continue
 
---
+++ piplmesh/panels/tasks.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+# We import panels through fake panel module here so that Django finds their models, tasks, etc.
+from . import panel
