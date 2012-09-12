e2c8de1cdb3515ec1250e5776eac9f2282e9bad0
Lazar Milinkovic <lazarmilinkovic05@gmail.com>
2012-09-12 12:56:26 +0200

    Switched back to upsert, edited TODO

--- piplmesh/panels/bicikelj/tasks.py
+++ piplmesh/panels/bicikelj/tasks.py
@@ -8,9 +8,15 @@ from . import models, stations
 @task.task
 def update_station_info():
     for station in stations.fetch_data():
-        station_object, created = models.BicikeljStation.objects.get_or_create(
-            station_id=station['station_id'],
-            timestamp=station['timestamp'],
-            defaults=station,
-        )
-        # TODO: if created, send an update to server, otherwise update fetch_time
+        models.BicikeljStation.objects(station_id=station['station_id'], timestamp=station['timestamp']).update(
+            set__name=station['name'],
+            set__address=station['address'],
+            set__location=station['location'],
+            set__available=station['available'],
+            set__free=station['free'],
+            set__total=station['total'],
+            set__open=station['open'],
+            set__fetch_time=station['fetch_time'],
+            upsert=True)
+
+        # TODO: use get_or_create to know the result of the query. If created, send an update to server, otherwise just update fetch time
