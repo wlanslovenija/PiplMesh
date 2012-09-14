84c9fd34370969e8d62ca5db7f5d3823f1848b8a
Tomaz <dcrystalj@gmail.com>
2012-09-14 13:15:55 +0200

    added jqXHR parameter

--- piplmesh/frontend/static/piplmesh/js/about.js
+++ piplmesh/frontend/static/piplmesh/js/about.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.getJSON('https://api.github.com/repos/wlanslovenija/PiplMesh/contributors', function (data, textStatus) { 
+    $.getJSON('https://api.github.com/repos/wlanslovenija/PiplMesh/contributors', function (data, textStatus, jqXHR) { 
         var contributors = $('<ul/>');
         $.each(data, function(i, contributor){
             contributors.append( 
