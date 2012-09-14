e5539bffeafb3e7b6f2f149c744e971a052abe7c
Gitek1 <denis.zupanc@gmail.com>
2012-09-14 19:50:52 +0200

    Fixed:  When we click button close of advanced map, the first element which becomes hidden is overlay ---> it looks better  Copied external files to our static directory.  Created div basic-map-extend-img instead img class basic-map-extend-img and shown img close button with JQuery.  Used the icon close button from Visage and set it in the right top corner of the window instead of on the map in the corner, like it was before.  Made layer background less transparent and fade in and fade out effect.  Added function, which makes the map close when we click outside of the map.  Fixed style of code: ($('#map').detach().prependTo('#basic-map')) to $('#map').detach().prependTo('#basic-map')  Used method hide() instead of method css to set div visibility to false:  $('#basic-map-extend-img').css('visibility', 'hidden'); to >   $('#basic-map-extend-img').hide();  Used method show() instead of method css to set div visibility to true:   $('#basic-map-extend-img').css('visibility', 'visible'); to $('#basic-map-extend-img').show();  When we resize map, the marker stays still in center of the map.
    To be continued...;)

--- piplmesh/panels/map/static/piplmesh/panel/map/panel.js
+++ piplmesh/panels/map/static/piplmesh/panel/map/panel.js
@@ -11,13 +11,9 @@ $(document).ready(function (event) {
         configureAdvancedMap();
         openAdvancedMap();
     });
-    function getStyle(el, styleProp){
-        var value = $(el).css(styleProp);
-        return value;
-    }
     function defineMap(){
         nodeLocation = new google.maps.LatLng(node.latitude, node.longitude);
-        myOptions = {
+        mapControls = {
             'zoom': 12,
             'center': nodeLocation,
             'scrollwheel': false,
@@ -27,7 +23,8 @@ $(document).ready(function (event) {
             'mapTypeId': google.maps.MapTypeId.ROADMAP,
             'streetViewControl': false
         };
-        map = new google.maps.Map($("#map")[0], myOptions);
+        map = new google.maps.Map($("#map")[0], mapControls);
+        $('#basic-map-extend-img').css("background-image", imageExtendUrl);
         var marker = new google.maps.Marker({
                 'position': nodeLocation,
                 'map': map,
@@ -35,41 +32,55 @@ $(document).ready(function (event) {
         });
     }
     function configureAdvancedMap(){
-        map.navigationControl = true;
-        map.scaleControl = true;
-        map.draggable = true;
-        map.streetViewControl = true;
+        map.set('scrollwheel',true);
+        map.set('navigationControl',true);
+        map.set('scaleControl', true);
+        map.set('draggable', true);
+        map.set('streetViewControl', true);
     }
     function configureBasicMap(){
+        map.set('scrollwheel',false);
         map.set('navigationControl',false);
         map.set('scaleControl', false);
         map.set('draggable', false);
         map.set('streetViewControl', false);
     }
     function closeAdvancedMap(){
-        $('#advanced-map').animate({
+        $('#closeButton').remove();
+        $('#overlay').fadeOut('slow', 0.0, function() {
+            // Animation complete.
+            $('#overlay').remove();
+        });
+        $('#advanced-map').animate ({
             width: '-=86%'
         }, 500, "linear", function (event){
             //callback
-            ($('#map').detach().prependTo('#basic-map')).insertAfter('#map-info');
-            $('#overlay').remove();
+            $('#map').detach().prependTo('#basic-map').insertAfter('#map-info');
             $('#advanced-map').remove();
-            $('#basic-map-extend-img').css('visibility', 'visible');
+            $('#basic-map-extend-img').show();
+            refreshMapCenter();
             configureBasicMap();
         });
     }
+    function refreshMapCenter(){
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
+    }
     function openAdvancedMap(){
-        $('#basic-map-extend-img').css('visibility', 'hidden');
+        $('#basic-map-extend-img').hide();
         $('<div/>').attr('id', 'advanced-map').appendTo(document.body);
-        $('<div/>').attr('id', 'overlay').appendTo(document.body);
+        $('<div/>').attr('id', 'overlay').fadeTo("fast", 0.8, function (){
+        //Animation complete
+        }).appendTo(document.body).click(closeAdvancedMap);
+        $('<div/>').attr('id', 'closeButton').appendTo(document.body).click(closeAdvancedMap);
+        $('#closeButton').css("background-image", imageCloseUrl);
         $('#advanced-map').append($('<div/>').attr('id', 'advanced-map-container'));
-        $('#advanced-map').append($('<a/>').addClass('close').click(closeAdvancedMap));
-      //$('#close')[0].style.backgroundImage = imageUrl;
         $('#map').detach().prependTo('#advanced-map-container');
-        $('#advanced-map').animate({
+        $('#advanced-map').animate ({
             width: '86%'
         }, 500, "linear", function (event){
-            google.maps.event.trigger(map, 'resize');
+            refreshMapCenter();
         });
         $(document).keyup(function (event) {
             //  alert(e.which);
