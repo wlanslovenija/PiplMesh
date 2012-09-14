9fb59705b61d60ca395f62378d130eb864314afa
Tomaz <dcrystalj@gmail.com>
2012-09-14 13:26:52 +0200

    added space .

--- piplmesh/frontend/static/piplmesh/js/about.js
+++ piplmesh/frontend/static/piplmesh/js/about.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $.getJSON('https://api.github.com/repos/wlanslovenija/PiplMesh/contributors', function (data, textStatus, jqXHR) { 
         var contributors = $('<ul/>');
-        $.each(data, function(i, contributor){
+        $.each(data, function (i, contributor) {
             contributors.append( 
                 $('<li/>').append(
                     $('<img/>').prop({
