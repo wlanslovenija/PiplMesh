b038b99ccee4705a3aa5257c81d9460ee3bc71f0
Tomaz <dcrystalj@gmail.com>
2012-09-14 12:55:33 +0200

    update2

--- piplmesh/frontend/static/piplmesh/js/about.js
+++ piplmesh/frontend/static/piplmesh/js/about.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-    $.getJSON('https://api.github.com/repos/wlanslovenija/PiplMesh/contributors', function(data) { 
+    $.getJSON('https://api.github.com/repos/wlanslovenija/PiplMesh/contributors', function (data, textStatus) { 
         var contributors = $('<ul/>');
         $.each(data, function(i, contributor){
-            var contributor = $('<li/>').append(
-                $('<img/>').prop({
-                    'src': contributor.avatar_url,
-                    'alt': gettext("contributor github picture")
-                })
-            ).append(
-                $('<a/>').prop({
-                    'href': 'https://github.com/' + contributor.login                    
-                }).append(contributor.login)
+            contributors.append( 
+                $('<li/>').append(
+                    $('<img/>').prop({
+                        'src': contributor.avatar_url,
+                        'alt': gettext("contributor github picture")
+                    })
+                ).append(
+                    $('<a/>').prop({
+                        'href': 'https://github.com/' + contributor.login                    
+                    }).append(contributor.login)
+                )
             );
-            contributors.append(contributor);
         });
         $('.contributors').append(contributors);
     }); 
