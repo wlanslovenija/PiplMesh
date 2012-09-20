47c02efa8ef44c58cc054ee39cc208f88bacb07d
Mitar <mitar.git@tnode.com>
2012-09-19 20:57:33 -0700

    Moved all forms JavaScript code to common code.

--- piplmesh/frontend/static/piplmesh/js/forms.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var max = 0;
-    $('.field label.main').each(function (index, element) {
-        if ($(this).width() > max)
-            max = $(this).width();
-    });
-    $('.field label.main').width(max);
-    $('.align_to_label_width').css('margin-left', max );
-});
