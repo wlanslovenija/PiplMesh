a1e47746f4f454d82c2cd954314fbdadf3b26d6e
mitjap <mitjap@mitjap-ubuntu.(none)>
2012-09-25 00:52:44 +0200

    Added OpenStreetMap layer to map (#215)

--- piplmesh/panels/map/static/piplmesh/panel/map/panel.js
+++ piplmesh/panels/map/static/piplmesh/panel/map/panel.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+    var element = document.getElementById('map_canvas');
     var nodeLocation = new google.maps.LatLng(node.latitude, node.longitude);
+
     var myOptions = {
         zoom: 15,
         center: nodeLocation,
@@ -7,17 +9,37 @@ $(document).ready(function () {
         navigationControl: false,
         scaleControl: false,
         draggable: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeId: "OSM",
+        mapTypeControlOptions: {
+            mapTypeIds: [
+                "OSM",
+                google.maps.MapTypeId.ROADMAP,
+                google.maps.MapTypeId.SATELLITE
+            ]
+        },
         streetViewControl: false
     };
-    var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-    var nodeName = $('<p/>').text(node.name).append(' | ');
-    var nodeWebsite = $('<a/>').prop('href', node.url).text(gettext("more info"));
-    nodeName.append(nodeWebsite);
-    $('#map_info').append(nodeName);
+
+    var map = new google.maps.Map(element, myOptions);
+
+    map.mapTypes.set("OSM", new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            return "http://tile.openstreetmap.org/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+        },
+        tileSize: new google.maps.Size(256, 256),
+        name: "OpenStreetMap",
+        maxZoom: 18
+    }));
+
     var marker = new google.maps.Marker({
         position: nodeLocation,
         map: map,
         title: node.name
     });
+
+    var nodeName = $('<p/>').text(node.name).append(' | ');
+    var nodeWebsite = $('<a/>').prop('href', node.url).text(gettext("more info"));
+    nodeName.append(nodeWebsite);
+    $('#map_info').append(nodeName);
+
 });
