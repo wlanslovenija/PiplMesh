cb848b252c09898bbd9de0be369451d32f86f433
mitjap <mitjap@mitjap-ubuntu.(none)>
2012-09-25 14:18:23 +0200

    Minor changes on map panel (javascript changes)

--- piplmesh/panels/map/static/piplmesh/panel/map/panel.js
+++ piplmesh/panels/map/static/piplmesh/panel/map/panel.js
@@ -1,45 +1,43 @@
 $(document).ready(function () {
-    var element = document.getElementById('map_canvas');
     var nodeLocation = new google.maps.LatLng(node.latitude, node.longitude);
 
     var myOptions = {
-        zoom: 15,
-        center: nodeLocation,
-        scrollwheel: false,
-        navigationControl: false,
-        scaleControl: false,
-        draggable: false,
-        mapTypeId: "OSM",
-        mapTypeControlOptions: {
-            mapTypeIds: [
-                "OSM",
+        'zoom': 15,
+        'center': nodeLocation,
+        'scrollwheel': false,
+        'navigationControl': false,
+        'scaleControl': false,
+        'draggable': false,
+        'mapTypeId': 'OpenStreetMap',
+        'mapTypeControlOptions': {
+            'mapTypeIds': [
+                'OpenStreetMap',
                 google.maps.MapTypeId.ROADMAP,
                 google.maps.MapTypeId.SATELLITE
             ]
         },
-        streetViewControl: false
+        'streetViewControl': false
     };
 
-    var map = new google.maps.Map(element, myOptions);
+    var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
 
-    map.mapTypes.set("OSM", new google.maps.ImageMapType({
-        getTileUrl: function(coord, zoom) {
-            return "http://tile.openstreetmap.org/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+    map.mapTypes.set('OpenStreetMap', new google.maps.ImageMapType({
+        getTileUrl: function(coordinates, zoom) {
+            return 'http://tile.openstreetmap.org/' + zoom + '/' + coordinates.x + '/' + coordinates.y + '.png';
         },
-        tileSize: new google.maps.Size(256, 256),
-        name: "OpenStreetMap",
-        maxZoom: 18
+        'tileSize': new google.maps.Size(256, 256),
+        'name': 'OpenStreetMap',
+        'maxZoom': 18
     }));
 
     var marker = new google.maps.Marker({
-        position: nodeLocation,
-        map: map,
-        title: node.name
+        'position': nodeLocation,
+        'map': map,
+        'title': node.name
     });
 
     var nodeName = $('<p/>').text(node.name).append(' | ');
-    var nodeWebsite = $('<a/>').prop('href', node.url).text(gettext("more info"));
+    var nodeWebsite = $('<a/>').prop('href', node.url).text(gettext('more info'));
     nodeName.append(nodeWebsite);
     $('#map_info').append(nodeName);
-
 });
