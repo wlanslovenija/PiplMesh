994e47a856b1e5caeb4574abb9e62dc10ad4d792
Tomaz <dcrystalj@gmail.com>
2012-09-26 16:08:06 +0200

    updated design

--- piplmesh/panels/weather/panel.py
+++ piplmesh/panels/weather/panel.py
@@ -14,24 +14,33 @@ FORECAST_HOUR = 14 # 14:00 o'clock or 2:00 PM
 
 class WeatherPanel(panels.BasePanel): 
     def get_context(self, context):
+        context.update({
+            'header': _("Weather"),
+        })
+        
         latitude = nodes.get_node(self.request).latitude
         longitude = nodes.get_node(self.request).longitude
-        state = models.State.objects(latitude=latitude, longitude=longitude).order_by('+created').first()
+        state = models.State.objects(latitude=latitude, longitude=longitude, at__lte=datetime.datetime.now(), at__gte=(datetime.datetime.now() - datetime.timedelta(hours=WEATHER_OBSOLETE))).order_by('+created').first()
         if state is None:
             context.update({
                 'error_data': True,
             })
             return context
+        
+        if timezone.now() > state.created + datetime.timedelta(hours=WEATHER_OBSOLETE):
+            context.update({
+                'error_obsolete': True,
+            })
+            return context   
             
         context.update({
-            'header': _("Weather"),
             'weather_objects': get_weather_content(latitude, longitude),
             'created': state.created,
         })
         return context
 
 def get_weather_content(latitude, longitude):
-    date = timezone.now()
+    date = datetime.datetime.now()
     for interval in range(0, 3):
         state = models.State.objects(latitude=latitude, longitude=longitude, at__lte=date).order_by('-at').first()
         precipitation = models.Precipitation.objects(latitude=latitude, longitude=longitude, date_from__lte=date, date_to__gte=date).order_by('-date_from').first()
@@ -39,7 +48,7 @@ def get_weather_content(latitude, longitude):
             'at': state.at,
             'temperature':  state.temperature,
             'symbol':  precipitation.symbol,
-        }      
+        }
         date += datetime.timedelta(days=1)
         date = date.replace(hour=FORECAST_HOUR, minute=0)
         yield weather_object
