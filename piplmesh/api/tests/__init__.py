9b7cdccea2cc5e21285fe2f4c2c3fd863d1bd305
Mitar <mitar.git@tnode.com>
2012-10-02 14:51:34 -0700

    Fixed test auto-discovery.

--- piplmesh/api/tests/__init__.py
+++ piplmesh/api/tests/__init__.py
@@ -4,5 +4,8 @@ from django.utils import unittest
 
 def suite():
     return unittest.TestSuite((
-        unittest.TestLoader().discover(os.path.abspath(os.path.dirname(__file__))),
-        ))
+        unittest.TestLoader().discover(
+            start_dir=os.path.abspath(os.path.dirname(__file__)),
+            top_level_dir=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')),
+        ),
+    ))
